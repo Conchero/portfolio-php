f6655bc0ae8c326bfce504bd1f18d27d3fd109b5
florian.parra@hotmail.com <florian.parra@hotmail.com>
2025-04-09 15:08:49 +0200

    port

--- assets/validateForm.js
+++ assets/validateForm.js
@@ -1,13 +1,17 @@
 
 
+
+
+
 const inputs = document.querySelectorAll("input");
+const textAreas = document.querySelectorAll("textarea");
 const errorMessage = document.querySelectorAll(".error_message");
 
 inputs.forEach((el, i) => {
 
     el.addEventListener("keyup", (e) => {
 
-        console.log(e.target.value);
+        console.log(e.target.value.length);
         if (el.minLength > 0 && e.target.value.length < el.minLength) {
             errorMessage[i].innerHTML = `Veuillez entrer au minimum ${el.minLength} caractére`;
         }
--- templates/contact.php
+++ templates/contact.php
@@ -1,20 +1,20 @@
 <?php
+ $firstName = htmlspecialchars($_POST["first-name__input"] );
+ $lastName = htmlspecialchars($_POST["last-name__input"] );
 
-
-// $str = "This is some <b>bold</b> text.";
-// $sanitizedStr = htmlspecialchars($str);
-
-// $firstName = htmlspecialchars($_POST["first-name__input"] );
-// $lastName = htmlspecialchars($_POST["last-name__input"] );
+ var_dump($firstName);
 ?>
 
 
 
 
 <h1 class="bg-clip-text bg-gradient-to-br from-slate-800 to-slate-400 text-6xl font-bold text-transparent text-center">
-    Me Contactere
+    Me Contacter
 </h1>
 
+
+
+<script src="../assets/validateForm.js" defer></script>
 
 
 <?php
